414cdce0137d159ea654d7df0800517ad6d5e45f
yo30M <129662479+yo30M@users.noreply.github.com>
2023-07-20 02:59:38 +0000

    Add Tags

--- db/migrate/20230720013250_add_missing_unique_indices.acts_as_taggable_on_engine.rb
+++ db/migrate/20230720013250_add_missing_unique_indices.acts_as_taggable_on_engine.rb
@@ -3,7 +3,7 @@
 
 class AddMissingUniqueIndices < ActiveRecord::Migration[6.0]
   def self.up
-    add_index ActsAsTaggableOn.tags_table, :name
+    add_index ActsAsTaggableOn.tags_table, :name, unique: true
 
     remove_index ActsAsTaggableOn.taggings_table, :tag_id if index_exists?(ActsAsTaggableOn.taggings_table, :tag_id)
     remove_index ActsAsTaggableOn.taggings_table, name: 'taggings_taggable_context_idx'
