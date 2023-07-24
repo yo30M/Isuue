6c3883c0c27256e47c2cbdf4173b0fd59ea3c7d0
yo30M <129662479+yo30M@users.noreply.github.com>
2023-07-24 01:09:26 +0000

    Finish Admin's Posts&Comments

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,5 +1,10 @@
 class Admin::CommentsController < ApplicationController
 
+  def destroy
+    Comment.find(params[:id]).destroy
+    redirect_to admin_post_path(params[:post_id])
+  end
+
   private
 
   def comment_params
--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,12 +1,21 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
 
   def index
+    @posts = Post.all
+    if params[:tag_name]
+      @posts = Post.tagged_with("#{params[:tag_name]}")
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to admin_posts_path
   end
 
   private
