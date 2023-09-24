f926506aa673730d47e5ba710bda0be51c0c6a6e
yo30M <129662479+yo30M@users.noreply.github.com>
2023-09-24 06:11:04 +0000

    Comment　非同期通信化

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,16 +1,17 @@
 class Public::CommentsController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
-    comment.save
-    redirect_to post_path(post)
+    @post = Post.find(params[:post_id])
+    @user = @post.user
+    @comment = current_user.comments.new(comment_params)
+    @comment.post_id = @post.id
+    @comment.save
   end
 
   def destroy
-    Comment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
+    @comment = Comment.find(params[:id]).destroy
+    @comment.destroy
+    @post = Post.find(params[:post_id])
   end
 
   private
